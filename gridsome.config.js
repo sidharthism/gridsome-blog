dfcd6a2cd93c085f4d0d3ffc2c1b1e7e7b0c8628
Sidharth S <siddhuinchakattu@gmail.com>
2020-08-12 00:01:36 +0530

    initial setup

---
+++ gridsome.config.js
@@ -0,0 +1,22 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'My blog',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'articles/**/*.md',
+        typeName: 'Articles',
+        remark: {}
+      }
+    },
+  ],
+  transformers: {
+    remark: {}
+  }
+}
